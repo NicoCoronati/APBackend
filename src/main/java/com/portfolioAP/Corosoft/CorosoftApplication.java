cf1891b679d3d654a7c9f4cb5ab3b725a1fb4129
NicoCoronati <74803821+NicoCoronati@users.noreply.github.com>
2022-08-29 18:33:48 -0300

    agregado ed a Requested

--- src/main/java/com/portfolioAP/Corosoft/CorosoftApplication.java
+++ src/main/java/com/portfolioAP/Corosoft/CorosoftApplication.java
@@ -21,7 +21,7 @@ public class CorosoftApplication {
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfolio-ap-df992.web.app/"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-                "Accept", "Authorization", "Origin, Accept", "X-Request-With",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
                 "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
