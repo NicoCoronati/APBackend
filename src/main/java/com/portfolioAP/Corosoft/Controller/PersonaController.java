4a83da4fd205836d6e4b1e811dbdf9a8a362db75
NicoCoronati <74803821+NicoCoronati@users.noreply.github.com>
2023-01-20 01:10:25 -0300

    edit controller persona

--- src/main/java/com/portfolioAP/Corosoft/Controller/PersonaController.java
+++ src/main/java/com/portfolioAP/Corosoft/Controller/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class PersonaController {
     @Autowired 
     private IPersonaService ipersonaService;
